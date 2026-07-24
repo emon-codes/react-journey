4745304b3f58acad341bb4e0b0649c725f981030
emon ahmed <emon9996a@gmail.com>
2026-07-24 22:23:17 +0600

    router setup and create pages

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../Components/Header'
+import { Outlet } from 'react-router'
+import Footer from '../Components/Footer'
+
+const Root = () => {
+  return (
+      <>
+          <Header></Header>
+          <Outlet />
+          <Footer/>
+      
+      </>
+  )
+}
+
+export default Root
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import './index.css'
+
+
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Root from './Layout/Root';
+import Home from './pages/Home';
+import About from './pages/About';
+import ContactsUs from './pages/ContactsUs';
+import Blog from './pages/Blog';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    children: [
+      { index: true, element: <Home/> },
+      { path: "/about", element: <About/> },
+      { path: "/contactsUs", element: <ContactsUs/> },
+      { path: "/blog", element: <Blog/> },
+    
+    ],
+  },
+]);
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
+);
+
+
+

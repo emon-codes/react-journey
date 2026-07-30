ce3b212aac4b88721b8d91e3dba40a228dd89aed
emon ahmed <emon9996a@gmail.com>
2026-07-30 22:29:29 +0600

    json file update

--- src/Components/SectionHeading.jsx
+++ src/Components/SectionHeading.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const SectionHeading = ({}) => {
   return (
+    <div>
       <div>
-          
+        <div></div>
+        <div></div>
+      </div>
 
-
-          
+      <div></div>
     </div>
-  )
-}
+  );
+};
 
-export default SectionHeading
+export default SectionHeading;

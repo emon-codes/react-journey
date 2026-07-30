ecb09d83ffc2a41fc83b7a5fb8be19b99486816c
emon ahmed <emon9996a@gmail.com>
2026-07-30 22:52:58 +0600

    SecionHeading complite

--- src/Components/SectionHeading.jsx
+++ src/Components/SectionHeading.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import useData from "../Hooks/useData";
 
-const SectionHeading = () => {
+const SectionHeading = ({heading, colorHeading, headingDescription}) => {
   const {products} = useData()
   return (
     <div>
-      <div>
-        <div className="flex flex-col">
-          <h3>
-            Propulr<span className="text-yello-600 font-800 text-4xl">products</span>
+      <div className="flex flex-row justify-between mx-5">
+        <div className="flex flex-col gap-2">
+          <h3 className="font-800 text-4xl">
+            {heading}<span className="text-green-400">{colorHeading}</span>
           </h3>
-          <p className="text-gray-400 ">Shop online for new arrivals and get free shoping!</p>
+          <p className="text-gray-400 ">{headingDescription}</p>
         </div>
-        <div>
+        <div className="flex gap-2 items-center">
+          {products.slice(0, 5).map((product) => (
+            <div>
+              <span>
+                {product.categoryName}
+              </span>
+            </div>
 
+
+          ))}
 
 
         </div>
--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../Components/Header'
 import { Outlet } from 'react-router'
 import Footer from '../Components/Footer'
+import SectionHeading from '../Components/SectionHeading'
 
 const Root = () => {
   return (
       <>
           <Header></Header>
           <Outlet />
+          <SectionHeading/>
           <Footer/>
       
       </>
